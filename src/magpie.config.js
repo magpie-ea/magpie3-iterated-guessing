6e794cd9d9fc28005db12024f176f233464ace7d
michael-franke <the.secret.goldfish@me.com>
2022-04-27 16:05:44 +0200

    updated deploy information for testing

--- src/magpie.config.js
+++ src/magpie.config.js
@@ -1,10 +1,10 @@
 export default {
-  experimentId: '84',
+  experimentId: '86',
   serverUrl: 'https://mcmpact.ikw.uni-osnabrueck.de/magpie',
   socketUrl: 'wss://mcmpact.ikw.uni-osnabrueck.de/magpie/socket',
   // this will be used in prolific mode
-  completionUrl: 'https://...',
+  completionUrl: 'https://app.prolific.co/submissions/complete?cc=1C9A8D5A',
   contactEmail: 'mchfranke@gmail.com',
   // Either 'debug', 'directLink' or 'prolific'
-  mode: 'directLink'
+  mode: 'prolific'
 };
